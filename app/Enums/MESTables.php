16948b78dd0eab98f202d60db17fe503e4f534a3
swolley <myentity@inwind.it>
2026-07-15 00:44:44 +0200

    feat(mes): Enhance MESTables enum with utility traits
    - Added HasModuleTablesUtils trait to MESTables enum for improved functionality and utility methods.

--- app/Enums/MESTables.php
+++ app/Enums/MESTables.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\MES\Enums;
 
+use Modules\Core\Enums\Concerns\HasModuleTablesUtils;
+
 enum MESTables: string
 {
+    use HasModuleTablesUtils;
+    
     case WorkCenters = 'mes_work_centers';
     case WorkCenterCalendars = 'mes_work_center_calendars';
     case Boms = 'mes_boms';
